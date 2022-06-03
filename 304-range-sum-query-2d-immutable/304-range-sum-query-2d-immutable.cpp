9672c1f0f3664c22e0e5f71583270b61c7e56367
Ayush Singh <84165258+aysayush@users.noreply.github.com>
2022-06-03 16:15:20 +0530

    Time: 555 ms (61.64%), Space: 148.1 MB (48.25%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -2,13 +2,16 @@ class NumMatrix {
 public:
    
     vector<vector<int>> sum1;
-    NumMatrix(vector<vector<int>>& matrix) {
+    NumMatrix(vector<vector<int>>& matrix) {  // void function will return null.
         
         int x = matrix.size(), y = matrix[0].size();
         sum1 = vector<vector<int>>(x + 1, vector<int>(y + 1)); 
         
+         // sum1 is a vector of vector with inner vector of length y+1 and outer vector             is of length x+1 initialised with zero.
+        
         for (int i = 1; i <= x; ++i) {
             for (int j = 1; j <= y; ++j) {
+                // prefix sum
                 sum1[i][j] = sum1[i - 1][j] + sum1[i][j - 1] - sum1[i - 1][j - 1] +                       matrix[i - 1][j - 1];
             }
         }
@@ -16,7 +19,9 @@ public:
     
     int sumRegion(int r1, int c1, int r2, int c2) {
 		
-        r1++; c1++; r2++; c2++; 
+        r1++; c1++; r2++; c2++;  
+    // formula (observation)
+        
  int ans= sum1[r2][c2] - sum1[r2][c1 - 1] - sum1[r1 - 1][c2] + sum1[r1 - 1][c1 - 1];
         
     return ans; }
