16d4305a473a8f99ea6750f725eb9bbb3095f615
Ayush Singh <84165258+aysayush@users.noreply.github.com>
2022-06-20 17:59:20 +0530

    Time: 406 ms (7.93%), Space: 14.3 MB (93.56%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    
+    // AYUSH 
+    
+    int minimumLengthEncoding(vector<string>& words) {
+        string ans;
+        sort(words.begin(),words.end(),[](string &a,string &b){return a.size() > b.size();});
+        for(string &s:words)
+            if(ans.find(s + "#") == string::npos) ans += s + "#";
+        return ans.size();
+    }
+};
