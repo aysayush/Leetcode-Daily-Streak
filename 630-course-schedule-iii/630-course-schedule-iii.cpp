cf7dc6342eef449515ad284be131d1ce72fa33cd
Ayush Singh <84165258+aysayush@users.noreply.github.com>
2022-06-24 20:42:25 +0530

    Time: 322 ms (63.47%), Space: 63.5 MB (30.03%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -5,9 +5,12 @@ public:
     
     int scheduleCourse(vector<vector<int>>& courses) {
         if(courses.size() <= 0) return 0;
-        sort(courses.begin(), courses.end(), [](const vector<int>& a, vector<int>& b) {
+        sort(courses.begin(), courses.end(), [](const vector<int>& a, vector<int>& b)     {
             return a[1] < b[1];
-        });
+        } ); 
+        
+    // comprator function for sorting the courses array on the basis                    of increasing order of their lastTime (u may use any other suitable function). 
+        
         priority_queue<int> q;
         int sum = 0;
         for(auto i : courses) {
