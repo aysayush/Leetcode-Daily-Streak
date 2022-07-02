37d70f10b094dbd384836be69c6b8a0284fdcd89
Ayush Singh <84165258+aysayush@users.noreply.github.com>
2022-07-02 23:58:46 +0530

    Time: 118 ms (31.79%), Space: 32.1 MB (95.37%) - LeetHub

---
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    // AYUSH
+    
+    int maxArea(int h, int w, vector<int>& hC, vector<int>& vC) {   
+        hC.push_back(h);
+        sort(hC.begin(), hC.end());
+		int maxh = hC[0];
+        for(int i=1; i<hC.size(); i++){
+            maxh = max(maxh, hC[i] - hC[i-1]);
+        }
+        
+        vC.push_back(w);
+        sort(vC.begin(), vC.end());
+		int maxv = vC[0];
+        for(int i=1; i<vC.size(); i++){
+            maxv = max(maxv, vC[i] - vC[i-1]);
+        }
+		
+        return (1LL*maxh*maxv) % 1000000007; //1LL used to make the product long long or integer sign overflow will occur.
+        
+    }
+};
