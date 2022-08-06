7517711933e22c5472a4a259f189421207f4fe5c
Ayush Singh <84165258+aysayush@users.noreply.github.com>
2022-08-06 19:56:31 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (22.55%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:  // AYUSH 
+   
+    int poorPigs(int buckets, int poisonTime, int totalTime) {
+    
+    // Total Rounds (T) = totalTime/poisonTime
+    // Assume p = no of pigs that will give us the result
+    // Therefore, (T+1)^p >= buckets
+    // Taking log on both sides, p = log(buckets)/log(T+1);
+    // Return ceil of p 
+    
+    return ceil(log(buckets) / log(totalTime / poisonTime + 1));
+        
+    }
+}; 
